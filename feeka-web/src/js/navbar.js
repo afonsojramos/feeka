20401a815571f15c482198f321deccaa4e78f9ba
Sofia Silva <up201505628@fe.up.pt>
2019-03-23 00:53:51 +0000

    minor :bug: fix

--- feeka-web/src/js/navbar.js
+++ feeka-web/src/js/navbar.js
@@ -1,9 +1,8 @@
-$(function () {
-    $(window).on("scroll", function () {
-        if ($(window).scrollTop() > 50) {
-            $(".navbar").addClass("active");
-        } else {
-            $(".navbar").removeClass("active");
-        }
-    });
-});
+window.onscroll = function () {
+    var element = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        element.classList.add("active");
+    } else {
+        element.classList.remove("active");
+    }
+};
